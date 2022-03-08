93c21a9b129dfa3e2e58e2919284b33164ee8f70
asraful7711 <asraful7711@gmail.com>
2022-03-08 23:50:04 +0600

    solve the reapeatation ,make the function smaller

--- js/banking.js
+++ js/banking.js
@@ -1,29 +1,46 @@
+// separate function for not repeatation 
+function getInputValue(inputId){
+    const inputFeild = document.getElementById(inputId);
+    const newInputText = inputFeild.value;
+    const newInputAmount = parseFloat (newInputText)
+
+    //    clear the deposite input field 
+    inputFeild.value = ''
+    return newInputAmount;
+ 
+}
+function addTotalFeild(totalId,Amount){
+    const totalFeild = document.getElementById(totalId)
+   const previousTotaltext = totalFeild.innerText
+   const previousTotalAmount = parseFloat(previousTotaltext)
+   const  newTotal = previousTotalAmount + Amount
+
+    totalFeild.innerText = newTotal
+    
+}
+function updateBalance( newDepositeAmount ,isAdd){
+     // update account balance 
+     const balanceTotal = document.getElementById('balance-total')
+     const BalanceTotalText = balanceTotal.innerText
+     const previousBalanceTotal = parseFloat(BalanceTotalText)
+    if(isAdd == true){
+     const newBalanceTotal = previousBalanceTotal + newDepositeAmount
+     balanceTotal.innerText = newBalanceTotal}
+   else{
+    const newBalanceTotal = previousBalanceTotal + newDepositeAmount
+    balanceTotal.innerText = newBalanceTotal
+   }
+}
+
+
 // handle deposite button 
 
 document.getElementById('deposit-button').addEventListener('click' , function(){
+    const newDepositeAmount = getInputValue('deposit-input');
+    
+    addTotalFeild('deposit-total',newDepositeAmount);
 
-   const depositeInput = document.getElementById('deposit-input');
-   const newDepositeText = depositeInput.value;
-   const newDepositeAmount = parseFloat (newDepositeText)
-
-
-   const depositeTotal = document.getElementById('deposit-total')
-   const previousDepositetext = depositeTotal.innerText
-   const previousDepositeAmount = parseFloat(previousDepositetext)
-   const  newDepositeTolat = previousDepositeAmount + newDepositeAmount
-
-    depositeTotal.innerText = newDepositeTolat
-
-
-    // update account balance 
-    const balanceTotal = document.getElementById('balance-total')
-    const BalanceTotalText = balanceTotal.innerText
-    const previousBalanceTotal = parseFloat(BalanceTotalText)
-    const newBalanceTotal = previousBalanceTotal + newDepositeAmount
-    balanceTotal.innerText = newBalanceTotal
-
-//    clear the deposite input field 
-    depositeInput.value = ''
+    updateBalance( newDepositeAmount,true)
 
 })
 
@@ -31,26 +48,10 @@ document.getElementById('deposit-button').addEventListener('click' , function(){
 
 document.getElementById('withdraw-button').addEventListener('click', function(){
 
-    const withdrawinput = document.getElementById('withdraw-input')
-    const newWithdrawText = withdrawinput.value;
-    const newWithdrawAmount = parseFloat(newWithdrawText)
-    
-    const withdrawTotal = document.getElementById('withdraw-total')
-    const withdrawTotalText = withdrawTotal.innerText
-    const previousWithdrawTotal = parseFloat(withdrawTotalText);
-    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount
-    withdrawTotal.innerText = newWithdrawTotal;
+    const newWithdrawAmount = getInputValue('withdraw-input');
 
-    // update balance
-    const balanceTotal = document.getElementById('balance-total')
-    const previousBalanceTotalText = balanceTotal.innerText
-    const previousBalanceTotal = parseFloat(previousBalanceTotalText)
-    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    addTotalFeild('withdraw-total',newWithdrawAmount);
 
-    balanceTotal.innerText = newBalanceTotal
+    updateBalance(  newWithdrawAmount ,false)
 
-
-
-//    clear the withdraw input field 
-    withdrawinput.value = ''
 })
